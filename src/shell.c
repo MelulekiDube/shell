eacdc21c2a3a8eea92d2958d8c7f6aab63527897
MelulekiDube <mellarmellah@gmail.com>
2019-08-08 11:31:18 +0000

    Fixed the issue caused by backspace

--- src/shell.c
+++ src/shell.c
@@ -7,9 +7,9 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-static char *curren_dir;
+static char *current_dir;
 void update_dir();
-
+static int typed_chars = 0;
 int memory_alloc_error(){
 	fprintf(stderr, "msh: allocation error\n");
 	exit(EXIT_FAILURE);
@@ -46,7 +46,7 @@ int msh_num_builtins(){
 
 
 
-/* 
+/*
 	Builtin function implementations.
 */
 int msh_cd(char **args){
@@ -69,11 +69,11 @@ int msh_help(char **args){
 	printf("Meluleki Dube Shell\n");
 	printf("Type the program names and arguments, then hit enter.\n");
 	printf("The following are built in:\n");
-	
+
 	for(i = 0; i< msh_num_builtins(); ++i){
 		printf(" %s\n", builtin_str[i]);
 	}
-	
+
 	printf("Use the man command for info on other prorgams.\n");
 	return 1;
 }
@@ -88,7 +88,7 @@ int msh_exit(char **_){
 //move readline part to new function to tidy code up
 #define MSH_RL_BUFFERSIZE 1024
 char *msh_read_line(void){
-	
+
 	int bufsize = MSH_RL_BUFFERSIZE;
 	int position = 0;
 	char *buffer = malloc(sizeof(char)*bufsize);
@@ -99,14 +99,20 @@ char *msh_read_line(void){
 		//read chars
 		c = getchar();
 		if(c==EOF || c=='\n'){
-			//place the string terminating char at eof or \n 
+			//place the string terminating char at eof or \n
 			buffer[position] = '\0';
 			return buffer;
-		}else{
+		}else if(c==8){
+            if(c>0){
+                --c;
+                buffer[position] = c;
+            }
+        }else{
+            ++typed_chars;
 			buffer[position] = c;
 		}
 		++position;
-		
+
 		//if we exceed the buffer, reallocate.
 		if(position>=bufsize){
 			bufsize += MSH_RL_BUFFERSIZE;
@@ -125,31 +131,31 @@ char **msh_split_line(char *line){
 	char *token;
 	if(!tokens)
 		memory_alloc_error();
-	
+
 	token = strtok(line, MSH_TOK_DELM);//returns the next token
 	while(token){
 		tokens[pos] = token;
 		++pos;
-		
+
 		if(pos >= bufsize){
 			bufsize += MSH_RL_BUFFERSIZE;
 			tokens = realloc(tokens, bufsize*sizeof(char*));
 			if(!tokens)
 				memory_alloc_error();
 		}
-		
+
 		token = strtok(NULL, MSH_TOK_DELM);
 	}
-	
+
 	tokens[pos] = NULL;
 	return tokens;
-	
+
 }
 
 int msh_launch(char **args){
 	pid_t pid, wpid;
 	int status;
-	
+
 	pid = fork();//pid =child
 	if(pid==0){
 		//child process
@@ -178,12 +184,12 @@ int msh_execute(char **args){
 		//empty command
 		return 1;
 	}
-	
+
 	for(i=0;i<msh_num_builtins();++i){
 		if(strcmp(args[0], builtin_str[i])==0){
 			return (*builtin_func[i])(args);
 		}
-	}	
+	}
 	return msh_launch(args);
 }
 
@@ -191,14 +197,14 @@ void msh_loop(){
 	char *line;
 	char **args;
 	int status;
-	
-	
+
+
 	do{
-		printf("%s> ",curren_dir);
-		line = readline("");
+		printf("%s> ",current_dir);
+		line = msh_read_line();
 		args = msh_split_line(line);
 		status = msh_execute(args);
-		
+
 		free(line);
 		free(args);
 	}while(status);
@@ -206,7 +212,7 @@ void msh_loop(){
 
 void update_dir(){
 	char cwd[PATH_MAX];
-	if(!(curren_dir = getcwd(cwd, sizeof(cwd))))
+	if(!(current_dir = getcwd(cwd, sizeof(cwd))))
 		*cwd = '\0';
 }
 
@@ -214,7 +220,7 @@ int main(int argc, char **argv){
 	//load config files
 	update_dir();
 	//Run command loop.
-	msh_loop();	
+	msh_loop();
 	//perform any shutdown or cleanup
 	return EXIT_SUCCESS;
 }
